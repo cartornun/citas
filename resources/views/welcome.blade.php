a891d67dc0478b8d1f93ae70b1d5675eb9a694bf
diego <dieencgal@alum.us.es>
2019-11-16 19:00:05 +0100

    Citas Pacientes TEAM SOCKET diego

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     CGIS: Citas Pacientes TEAM SOCKET
+                     CGIS: Citas Pacientes TEAM SOCKET diego
                 </div>
 
                 <div class="links">
