0af587a1c1aa19d25fa9f4e222a32b1ec0c23a51
diego <dieencgal@alum.us.es>
2019-11-19 10:55:13 +0100

    Creación de las migraciones y las relaciones de las entidades(Diego Encina Galnares)

---
+++ database/migrations/2019_11_18_101422_create_duracions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDuracionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('duracions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('fecha_inicio');
+            $table->dateTime('fecha_fin');
+
+            $table->unsignedInteger('cita_id');
+            $table->timestamps();
+
+            $table->foreign('cita_id')->references('id')->on('citas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('duracions');
+    }
+}
