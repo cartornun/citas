583cedabbc2c0efcea8393859fccb3e096cf5b00
diego <dieencgal@alum.us.es>
2019-11-26 16:00:38 +0100

    Creación de rutas y vista de enfermedades

--- resources/views/enfermedades/index.blade.php
+++ resources/views/enfermedades/index.blade.php
@@ -1,2 +1,49 @@
 
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Citas</div>
+
+                    <div class="panel-body">
+                        @include('flash::message')
+                        {!! Form::open(['route' => 'enfermedads.create', 'method' => 'get']) !!}
+                        {!!   Form::submit('Crear enfermedad', ['class'=> 'btn btn-primary'])!!}
+                        {!! Form::close() !!}
+
+                        <br><br>
+                        <table class="table table-striped table-bordered">
+                            <tr>
+                                <th>nombre</th>
+
+                                <th colspan="2">Acciones</th>
+                            </tr>
+
+                            @foreach ($enfermedads as $enfermedad)
+
+
+                                <tr>
+                                    <td>{{ $enfermedad->nombre }}</td>
+
+                                    <td>
+                                        {!! Form::open(['route' => ['enfermedads.edit',$enfermedad->id], 'method' => 'get']) !!}
+                                        {!!   Form::submit('Editar', ['class'=> 'btn btn-warning'])!!}
+                                        {!! Form::close() !!}
+                                    </td>
+                                    <td>
+                                        {!! Form::open(['route' => ['enfermedads.destroy',$enfermedad->id], 'method' => 'delete']) !!}
+                                        {!!   Form::submit('Borrar', ['class'=> 'btn btn-danger' ,'onclick' => 'if(!confirm("¿Está seguro?"))event.preventDefault();'])!!}
+                                        {!! Form::close() !!}
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
