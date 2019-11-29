2130a3ba5e6676cebd74a7404c3b7adafbd41f2f
diego <dieencgal@alum.us.es>
2019-11-29 14:18:49 +0100

    creacion vista de localizaciones creado por javier salazar moreno(Frasalmor1)

--- resources/views/localizaciones/create.blade.php
+++ resources/views/localizaciones/create.blade.php
@@ -1,1 +1,27 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Crear Localizacion</div>
+
+                    <div class="panel-body">
+                        @include('flash::message')
+
+                        {!! Form::open(['route' => 'localizaciones.store']) !!}
+                        <div class="form-group">
+                            {!! Form::label('lugar', 'Lugar de la localizacion') !!}
+                            {!! Form::text('lugar',null,['class'=>'form-control', 'required', 'autofocus']) !!}
+                        </div>
+
+                        {!! Form::submit('Guardar',['class'=>'btn-primary btn']) !!}
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/localizaciones/edit.blade.php
+++ resources/views/localizaciones/edit.blade.php
@@ -1,1 +1,33 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">editar localizacion</div>
+
+                    <div class="panel-body">
+                        @include('flash::message')
+
+                        {!! Form::model($localizacion, [ 'route' => ['localizaciones.update',$localizacion->id], 'method'=>'PUT']) !!}
+
+                        <div class="form-group">
+                            {!! Form::label('lugar', 'Lugar de la localizacion') !!}
+                            {!! Form::text('lugar',$lugar->name,['class'=>'form-control', 'required', 'autofocus']) !!}
+
+
+
+
+
+                        </div>
+
+                        {!! Form::submit('Guardar',['class'=>'btn-primary btn']) !!}
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
